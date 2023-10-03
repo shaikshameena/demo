40442699eb8fed8e565cccc3b6d0b84f11225820
shaikshameena <shaikshameena003@gmail.com>
2023-10-03 19:21:58 +0530

    givethefactorialofnum

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "factorial.h"
+
+int main(<command line args>)
+{
+    // Process command line argument for number
+    // Call factoial_num() which is declared and defined in factorial module.
+    // Print result.
+
+    return 0;
+}
