1663d89acedfdba9df9cd4517b9d9c9c240fbc46
shaikshameena <shaikshameena003@gmail.com>
2023-10-05 21:37:56 +0530

    final

--- factorial.c
+++ factorial.c
@@ -2,13 +2,15 @@
 
 int factorial(int number)
 {
-    if (number == 0)
-    {
-        return 1;
-    }
+     if (number == 0)    
+     {
+         return 1;
+     }
+     else 
+     {
+         return number * factorial(number - 1);
+     }
+}
+
     
-  else
-   {
-        return number * factorial(number - 1);
-   }
-  }
+
